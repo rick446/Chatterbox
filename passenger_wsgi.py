1e0c141ed1c3c98212347ef8d8b32bd73eeed6ac
Rick Copeland <rick@arborian.com>
2012-10-21 10:45:30 -0400

    add app
    Signed-off-by: Rick Copeland <rick@arborian.com>

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
+
 import sys, os
 
 # Switch to the virtualenv if we're not already there
 INTERP = os.path.expanduser("~/env/chatterbox/bin/python")
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
-os.chdir('Chatterbox')
+sys.path.append('./Chatterbox')
 import app
 application = app.application
