bb3e445798543e13de9976e1199b6405ecb83b58
Rick Copeland <rick@arborian.com>
2012-10-21 10:41:07 -0400

    passengerize
    Signed-off-by: Rick Copeland <rick@arborian.com>

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,4 +5,5 @@ INTERP = os.path.expanduser("~/env/chatterbox/bin/python")
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 os.chdir('Chatterbox')
-execfile('./wsgi-chat-socketio.py')
+import app
+application = app.application
